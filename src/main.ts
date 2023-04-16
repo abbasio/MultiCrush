1b8966bde74e054cd441fe2fcb7c8d378c868646
abbasio <omarabbasi16@gmail.com>
2023-04-16 12:41:39 -0400

    Added buttons to spawn different objects

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { crushScene } from './scenes/crush';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1280,
+  height: 720,
   physics: {
     default: 'matter',
     matter: {
--- src/scenes/build.ts
+++ src/scenes/build.ts
@@ -5,7 +5,7 @@ export class buildScene extends Phaser.Scene {
     super({ key: 'buildScene' });
   }
   preload() {
-    // Load sprite sheet generated with TexturePacker
+    // Load sprite sheet
     this.load.atlas(
       'sheet',
       'assets/block-sprites.png',
@@ -19,28 +19,68 @@ export class buildScene extends Phaser.Scene {
   create() {
     const Bodies = this.matter.bodies;
     const Shapes = this.cache.json.get('shapes');
+    let currentBlock = '';
 
     // Set up bodies
-    const ground = Bodies.rectangle(395, 600, 815, 50, {
+    const ground = Bodies.rectangle(900, 600, 815, 50, {
       isStatic: true,
       render: {},
     });
+    this.matter.world.add(ground);
 
-    const blockShape = {
-      shape: Shapes.block,
+    // Set up buttons
+    const blockButton = this.add.text(100, 100, 'Block', {});
+    blockButton
+      .setInteractive()
+      .on('pointerdown', () => {
+        currentBlock = 'block';
+      })
+      .on('pointerover', () => {
+        blockButton.setStyle({
+          color: '#0f0',
+        });
+      })
+      .on('pointerout', () => {
+        blockButton.setStyle({
+          color: '#fff',
+        });
+      });
+
+    const castleButton = this.add.text(300, 100, 'Castle', {});
+    castleButton
+      .setInteractive()
+      .on('pointerdown', () => {
+        currentBlock = 'castle';
+      })
+      .on('pointerover', () => {
+        castleButton.setStyle({
+          color: '#0f0',
+        });
+      })
+      .on('pointerout', () => {
+        castleButton.setStyle({
+          color: '#fff',
+        });
+      });
+
+    const createObjectOnClick = (pointer: any, key: string, block: string) => {
+      this.matter.add.image(pointer.x, pointer.y, key, block, {
+        shape: Shapes[block],
+      } as Phaser.Types.Physics.Matter.MatterBodyConfig);
     };
 
-    // Add bodies to scene
-    this.matter.world.add(ground);
     // Add block to scene on click
     this.input.on('pointerdown', (pointer) => {
-      this.matter.add.sprite(
-        pointer.x,
-        pointer.y,
-        'sheet',
-        'block',
-        blockShape as Phaser.Types.Physics.Matter.MatterBodyConfig
-      );
+      console.log(pointer.x);
+      if (currentBlock.length && pointer.x > 500) {
+        createObjectOnClick(pointer, 'sheet', currentBlock);
+      }
     });
+
+    //Things to try:
+    // Add buttons to generate different fort parts
+    // Add a save button
+    // On click, saves the x pos, y pos, and name, and config of each object into a json
+    // On loading the next scene, parse that info from the json and load the objects into the new scene
   }
 }
